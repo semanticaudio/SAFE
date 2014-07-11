18522d56993a7ea81da49008a1d23fdf0b7e2696
Sean Enderby <sean.enderby@gmail.com>
2014-07-11 10:53:00 +0100

    new Image files

--- JuceModules/SAFE_juce_module/LookAndFeel/SAFEImages.h
+++ JuceModules/SAFE_juce_module/LookAndFeel/SAFEImages.h
@@ -5,17 +5,26 @@
 
 namespace SAFEImages
 {
-    extern const char*  reverb_background_png;
-    const int           reverb_background_pngSize = 23677;
-    
     extern const char*  compressor_background_png;
-    const int           compressor_background_pngSize = 26101;
+    const int           compressor_background_pngSize = 27277;
 
     extern const char*  distortion_background_png;
-    const int           distortion_background_pngSize = 30216;
+    const int           distortion_background_pngSize = 31141;
 
     extern const char*  eq_background_png;
-    const int           eq_background_pngSize = 28435;
+    const int           eq_background_pngSize = 30288;
+
+    extern const char*  info_red_png;
+    const int           info_red_pngSize = 700;
+
+    extern const char*  info_red_mo_png;
+    const int           info_red_mo_pngSize = 737;
+
+    extern const char*  info_yellow_png;
+    const int           info_yellow_pngSize = 625;
+
+    extern const char*  info_yellow_mo_png;
+    const int           info_yellow_mo_pngSize = 738;
 
     extern const char*  knob_green_png;
     const int           knob_green_pngSize = 2203;
@@ -41,11 +50,14 @@ namespace SAFEImages
     extern const char*  record_png;
     const int           record_pngSize = 817;
 
+    extern const char*  recording_png;
+    const int           recording_pngSize = 1122;
+
     extern const char*  record_mo_png;
     const int           record_mo_pngSize = 867;
 
-    extern const char*  recording_png;
-    const int           recording_pngSize = 1122;
+    extern const char*  reverb_background_png;
+    const int           reverb_background_pngSize = 24942;
 
     extern const char*  save_png;
     const int           save_pngSize = 945;
